898e0148f1de359c02d7ec575fc335f0da8b632d
Justin Bennett <jbennett@scrippsnetworks.com>
2016-10-01 15:18:08 -0400

    Added example server multiplayer code

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port', port);
+});
+
+app.use(express.static(__dirname));
+
+let players = 0;
+
+io.on('connection', socket => {
+  socket.on('test', () => {
+    socket.emit('respond');
+  });
+  socket.on('connected', () => {
+    console.log('connected');
+  });
+  socket.on('disconnected', () => {
+    console.log('disconnected');
+  });
+});
